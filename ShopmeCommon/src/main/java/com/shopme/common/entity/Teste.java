b7054f15e1af23e7a1ccd67d18982260bf5bf247
JarmisonS10 <jarmison.paiva@varejonline.com.br>
2023-07-21 19:56:27 -0300

    commit inicial do projeto

---
+++ ShopmeCommon/src/main/java/com/shopme/common/entity/Teste.java
@@ -0,0 +1,10 @@
+package com.shopme.common.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Teste {
+    @Id
+    private Long idteste;
+}
---
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/ShopmeBackendRunner.java
@@ -0,0 +1,13 @@
+package com.shopme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+
+@SpringBootApplication
+@EntityScan({"com.shopme.common.entity"})
+public class ShopmeBackendRunner {
+    public static void main (String[] args){
+        SpringApplication.run(ShopmeBackendRunner.class,args);
+    }
+}
