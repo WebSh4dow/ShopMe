aa13eaa14b37765ec3d8daaf7e06e781d8e27a3a
JarmisonS10 <jarmison.paiva@varejonline.com.br>
2023-07-22 11:58:53 -0300

    TASK-4 camada de testes de usuários e cargos adicionados

---
+++ ShopmeCommon/src/main/java/com/shopme/common/entity/User.java
@@ -0,0 +1,56 @@
+package com.shopme.common.entity;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Data
+@Table(name = "users")
+@EqualsAndHashCode
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer code;
+
+    @Column(length = 150, nullable = false, unique = true)
+    private String email;
+
+    @Column(length = 64,nullable = false)
+    private String password;
+
+    @Column(name = "first_name",length = 150, nullable = false, unique = true)
+    private String firstName;
+
+    @Column(name = "last_name",length = 150, nullable = false, unique = true)
+    private String lastName;
+
+    @ManyToMany
+    @JoinTable(
+            name = "users_roles", joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id")
+    )
+    private Set<Roles> roles = new HashSet<>();
+
+    @Column(length = 64)
+    private String photos;
+
+    private boolean enabled;
+
+    public User(String email, String password, String firstName, String lastName) {
+        this.email = email;
+        this.password = password;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public User(){}
+
+    public void addRole(Roles roles){
+        this.roles.add(roles);
+    }
+
+}
---
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/user/UserRepository.java
@@ -0,0 +1,9 @@
+package com.shopme.user;
+
+import com.shopme.common.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User,Integer> {
+}
